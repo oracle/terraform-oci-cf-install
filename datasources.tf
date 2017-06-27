baa8d89367b7e990be8da9a071ea4a65ef7265a4
Devin Carlen <devin.carlen@gmail.com>
2017-06-26 17:26:44 -0700

    Fixed vnic attach data source

--- datasources.tf
+++ datasources.tf
@@ -5,7 +5,7 @@ data "baremetal_identity_availability_domains" "ADs" {
 
 # Gets a list of vNIC attachments on the instance
 data "baremetal_core_vnic_attachments" "InstanceVnics" {
-    compartment_id = "{baremetal_identity_compartment.bosh_compartment.id}"
+    compartment_id = "${baremetal_identity_compartment.bosh_compartment.id}"
     availability_domain = "${lookup(data.baremetal_identity_availability_domains.ADs.availability_domains[var.bastion_ad - 1], "name")}"
     instance_id = "${baremetal_core_instance.bosh_cli.id}"
 }
