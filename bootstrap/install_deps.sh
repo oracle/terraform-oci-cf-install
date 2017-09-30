a78865c426cc4d80efdf0a3faaa5ee4ef48b4304
Devin Carlen <devin.carlen@gmail.com>
2017-09-30 13:18:53 -0700

    Bump to BOSH CLI v2.0.39

--- bootstrap/install_deps.sh
+++ bootstrap/install_deps.sh
@@ -7,5 +7,5 @@ done
 
 sudo apt-get update -y
 sudo apt-get install -y make ruby
-sudo curl -o /usr/local/bin/bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.33-linux-amd64
+sudo curl -o /usr/local/bin/bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.39-linux-amd64
 sudo chmod +x /usr/local/bin/bosh
