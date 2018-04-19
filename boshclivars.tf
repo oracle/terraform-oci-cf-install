a42f5f38134bb4a97d9e1f476018c51e974e91e5
dmutreja <dhiraj.mutreja@oracle.com>
2018-04-19 12:57:15 -0700

    Fixes https://github.com/oracle/terraform-oci-cf-install/issues/7

--- boshclivars.tf
+++ boshclivars.tf
@@ -1,8 +1,9 @@
-resource "local_file"  bosh_cli_env_vars {
-    depends_on = ["oci_core_instance.bosh_cli"]
+resource "local_file" bosh_cli_env_vars {
+  depends_on = [
+    "oci_core_instance.bosh_cli"]
 
-    filename = "./bootstrap/my-env-vars.yml"
-    content = <<EOF
+  filename = "./bootstrap/my-env-vars.yml"
+  content = <<EOF
 tenancy: ${var.oci_tenancy_ocid}
 user: ${oci_identity_user.bosh_user.id}
 compartment: ${oci_identity_compartment.bosh_compartment.id}
@@ -10,9 +11,13 @@ apikey: |${format("\n   %s",join("\n   ", split("\n", file(var.bosh_api_private_
 fingerprint: ${file(var.bosh_api_fingerprint)}
 ssh_key: ${file(var.bosh_ssh_public_key)}
 vcn: ${oci_core_virtual_network.cloudfoundry_vcn.display_name}
+vcn_id: ${oci_core_virtual_network.cloudfoundry_vcn.id}
 ad1: ${oci_core_subnet.private_subnet_ad1.availability_domain}
 ad2: ${oci_core_subnet.private_subnet_ad2.availability_domain}
 ad3: ${oci_core_subnet.private_subnet_ad3.availability_domain}
+subnet1_id: ${oci_core_subnet.private_subnet_ad1.id}
+subnet2_id: ${oci_core_subnet.private_subnet_ad2.id}
+subnet3_id: ${oci_core_subnet.private_subnet_ad3.id}
 subnet1: ${oci_core_subnet.private_subnet_ad1.display_name}
 subnet2: ${oci_core_subnet.private_subnet_ad2.display_name}
 subnet3: ${oci_core_subnet.private_subnet_ad3.display_name}
@@ -26,11 +31,12 @@ EOF
 
 }
 
-resource "local_file"  bosh_cli_director_env_vars {
-    depends_on = ["oci_core_instance.bosh_cli"]
+resource "local_file" bosh_cli_director_env_vars {
+  depends_on = [
+    "oci_core_instance.bosh_cli"]
 
-    filename = "./bootstrap/director-env-vars.yml"
-    content = <<EOF
+  filename = "./bootstrap/director-env-vars.yml"
+  content = <<EOF
 tenancy: ${var.oci_tenancy_ocid}
 user: ${oci_identity_user.bosh_user.id}
 compartment: ${oci_identity_compartment.bosh_compartment.id}
@@ -41,7 +47,9 @@ ssh_key: ${file(var.bosh_ssh_public_key)}
 region: ${var.oci_region}
 ad: ${oci_core_subnet.director_subnet_ad1.availability_domain}
 vcn: ${oci_core_virtual_network.cloudfoundry_vcn.display_name}
+vcn_id: ${oci_core_virtual_network.cloudfoundry_vcn.id}
 subnet: ${oci_core_subnet.director_subnet_ad1.display_name}
+subnet_id: ${oci_core_subnet.director_subnet_ad1.id}
 internal_cidr: ${oci_core_subnet.director_subnet_ad1.cidr_block}
 internal_gw: ${cidrhost(oci_core_subnet.director_subnet_ad1.cidr_block, 1)}
 internal_ip: ${cidrhost(oci_core_subnet.director_subnet_ad1.cidr_block, 2)}
@@ -57,46 +65,49 @@ EOF
 }
 
 resource "null_resource" "copy-artifacts" {
-    depends_on = ["local_file.bosh_cli_env_vars", "local_file.bosh_cli_director_env_vars"]
+  depends_on = [
+    "local_file.bosh_cli_env_vars",
+    "local_file.bosh_cli_director_env_vars"]
+
+  connection {
+    agent = false
+    timeout = "2m"
+    host = "${data.oci_core_vnic.InstanceVnic.public_ip_address}"
+    user = "ubuntu"
+    private_key = "${file(var.bosh_ssh_private_key)}"
+  }
+
+  provisioner "file" {
+    source = "${local_file.bosh_cli_env_vars.filename}"
+    destination = "~/${basename(local_file.bosh_cli_env_vars.filename)}"
+  }
+
+  provisioner "file" {
+    source = "${local_file.bosh_cli_director_env_vars.filename}"
+    destination = "~/${basename(local_file.bosh_cli_director_env_vars.filename)}"
+  }
+
+  provisioner "file" {
+    source = "./keys/bosh-ssh"
+    destination = "~/.ssh/id_rsa"
+  }
+
+  provisioner "file" {
+    source = "./keys/bosh-ssh.pub"
+    destination = "~/.ssh/id_rsa.pub"
+  }
+
+  provisioner "remote-exec" {
+    on_failure = "continue"
 
     connection {
-        agent = false
-        timeout = "2m"
-        host = "${data.oci_core_vnic.InstanceVnic.public_ip_address}"
-        user = "ubuntu"
-        private_key = "${file(var.bosh_ssh_private_key)}"
+      agent = false
+      timeout = "30m"
+      host = "${data.oci_core_vnic.InstanceVnic.public_ip_address}"
+      user = "ubuntu"
+      private_key = "${file(var.bosh_ssh_private_key)}"
     }
-
-    provisioner "file" {
-        source = "${local_file.bosh_cli_env_vars.filename}"
-        destination = "~/${basename(local_file.bosh_cli_env_vars.filename)}"
-    }
-
-    provisioner "file" {
-        source = "${local_file.bosh_cli_director_env_vars.filename}"
-        destination = "~/${basename(local_file.bosh_cli_director_env_vars.filename)}"
-    }
-
-    provisioner "file" {
-        source = "./keys/bosh-ssh"
-        destination = "~/.ssh/id_rsa"
-    }
-
-    provisioner "file" {
-        source = "./keys/bosh-ssh.pub"
-        destination = "~/.ssh/id_rsa.pub"
-    }
-
-    provisioner "remote-exec" {
-        on_failure = "continue"
-
-        connection {
-          agent       = false
-          timeout     = "30m"
-          host        = "${data.oci_core_vnic.InstanceVnic.public_ip_address}"
-          user        = "ubuntu"
-          private_key = "${file(var.bosh_ssh_private_key)}"
-        }
-        inline = ["chmod 600 ~/.ssh/id_rsa"]
-    }
+    inline = [
+      "chmod 600 ~/.ssh/id_rsa"]
+  }
 }
